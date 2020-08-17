b5d5abd3d9269a19fd4b619a6f1c28f7f969e19f
Superchill93 <herosilas12@gmail.com>
2020-08-17 16:56:40 -0500

    added base GUI

--- GUI.py
+++ GUI.py
@@ -1,9 +1,30 @@
-import FX , wx
+import FX
+import tkinter as tk
 
-app = wx.App()
+def FormGUI():
 
-frm = wx.Frame(None, title='OpenRGB FX')
-frm.Show()
+    OpenRGB_FX = tk.Tk(screenName='OpenRGB FX',baseName='OpenRGB FX')
+    OpenRGB_FX.minsize(600,400)
+    OpenRGB_FX.title('OpenRGB FX')
 
+    SetModeFrame = tk.LabelFrame(None,text='Set Mode to')#,size=(200,200))
 
-app.MainLoop()
+    StaticButton = tk.Button(SetModeFrame,text='Static')
+    StaticButton.bind('<Button-1>',StaticButtonHandler)
+    StaticButton.pack()
+
+    SpectrumCycleButton = tk.Button(SetModeFrame,text='Spectrum Cycling')
+    SpectrumCycleButton.bind('<Button-1>',SpectrumCycleButtonHandler)
+    SpectrumCycleButton.pack()
+    
+    SetModeFrame.pack()
+
+    OpenRGB_FX.mainloop()
+
+def StaticButtonHandler(event):
+    FX.SetMode('static')
+
+def SpectrumCycleButtonHandler(event):
+    FX.SpectrumCycle()
+
+FormGUI()
