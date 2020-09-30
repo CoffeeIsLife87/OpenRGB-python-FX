a0eceb4070752a371bdfeed6b5cc5bc5587932fd
Superchill93 <herosilas12@gmail.com>
2020-09-30 08:18:01 -0500

    added Ambient

---
+++ Ambient.py
@@ -0,0 +1,71 @@
+import time, os
+from PIL import ImageGrab #pip3 install Pillow
+from colour import Color #pip3 install Colour
+from openrgb import OpenRGBClient #pip3 install openrgb-python
+from openrgb.utils import RGBColor
+ 
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////
+# GLOBAL DEFINES
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////
+#HEIGHT         = 1920   #now using image.size[1] dynamically
+#WIDTH          = 1200   #now using image.size[0] dynamically
+LOOP_INTERVAL  = 0.05    # how often we calculate screen colour (in seconds)
+DURATION       = 3    # how long it takes bulb to switch colours (in seconds)
+DECIMATE       = 10   # skip every DECIMATE number of pixels to speed up calculation
+
+
+cli = OpenRGBClient() #will only work if you use default ip/port for OpenRGB server. This is an easy fix, read the documentation if need be https://openrgb-python.readthedocs.io/en/latest/
+
+Dlist = cli.devices
+ 
+for device in Dlist:
+    try:
+        device.set_mode('direct')
+    except:
+        device.set_mode('static')
+        print("%s doesn't have direct mode. falling back to static"%device.name)
+    time.sleep(0.001)
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+ 
+ 
+# run loop
+while True:
+    #init counters/accumulators
+    red   = 0
+    green = 0
+    blue  = 0
+ 
+    time.sleep(LOOP_INTERVAL) #wake uAp ever so often and perform this ...
+    
+    
+    
+    #//////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # CALCULATE AVERAGE SCREEN COLOUR
+    #//////////////////////////////////////////////////////////////////////////////////////////////////////////
+    image = ImageGrab.grab()  # take a screenshot
+    #print image.size
+ 
+    
+    for y in range(0, image.size[1], DECIMATE):  #loop over the height
+        for x in range(0, image.size[0], DECIMATE):  #loop over the width
+            color = image.getpixel((x, y))  #grab a pixel
+            red = red + color[0]
+            green = green + color[1]
+            blue = blue + color[2]
+ 
+ 
+    red = (( red / ( (image.size[1]/DECIMATE) * (image.size[0]/DECIMATE) ) ) )/255.0
+    green = ((green / ( (image.size[1]/DECIMATE) * (image.size[0]/DECIMATE) ) ) )/255.0
+    blue = ((blue / ( (image.size[1]/DECIMATE) * (image.size[0]/DECIMATE) ) ) )/255.0
+    c= Color(rgb=(red, green, blue))  
+    print(c.rgb)
+    print(hex_to_rgb(c.hex))
+    RGBKB = hex_to_rgb(c.hex)
+
+    for D in Dlist:
+        D.set_color(RGBColor(RGBKB[0],RGBKB[1],RGBKB[2]))
+        time.sleep(0.001)
--- README.md
+++ README.md
@@ -10,6 +10,8 @@
 
 * Breathing
 
+* Ambient (credit to usrErr0r on discord for this one)
+
 ## Usage
 
 * start the openRGB sdk server, otherwise this *will not work*
@@ -24,17 +26,22 @@
 
 ### usage note
 
-Some effects support custom colors (gradcycle and breathing)
+* Some effects support custom colors (gradcycle and breathing)
 
-To use custom colors you must have 3(for breathing) or 6(for gradcycle) numbers ranging 0 to 255
+  * To use custom colors you must have 3(for breathing) or 6(for gradcycle) numbers ranging 0 to 255
 
-something like ```python(3) gradcycle.py 0 255 255 255 30 0``` or ```python(3) breathing.py 0 255 90```
+  * something like ```python(3) gradcycle.py 0 255 255 255 30 0``` or ```python(3) breathing.py 0 255 90```
+
+* Ambient requires some special python modules
+
+  * ```pip install pillow colour``` or ```pip3 install pillow colour``` (again, you may also need to add ```--user``` to the end for it to work)
 
 ## Writing effects
 
 Effects are stored in their own .py file for now
 
-```import openrgb , time , string , colorsys , sys
+```
+import openrgb , time , string , colorsys , sys
 from openrgb.utils import RGBColor , ModeData , DeviceType , ZoneType
 
 client = openrgb.OpenRGBClient()
@@ -51,3 +58,5 @@ While this is system wide, there are some limitations.
 if the device isn't in openRGB then the effect *will not* apply to it
 
 currently rainbow is poorly optimized due to a python sdk binding issue so it might be a bit jarring
+
+Ambient grabs the entire screen leading to some shade of white or black. hopefully this will get fixed soon
